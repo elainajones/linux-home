1d8a7c5ed71c902c5ec747c4d72b1c0de28883c4
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-12-31 03:44:11 -0800

    fix: Include /var/db/pkg in backup
    Includes vital /var/db/pkg in backup which is what informs portage
    of currently installed packaged. Since portage is unaware of what
    package versions are already present on the system and since these
    files are only updated at build time, there is no other solution
    than to re-emerge all impacted packages so that this info can be re-
    built.
    
    Because this information is used when calculating reverse dependencies,
    rebuilding by re-emerging impacted package sets (i.e. @world) often
    results in circular dependency errors which can be tedious to resolve
    manually with temporary USE and/or mask changes.
    
    The easiest way to rebuild is to simply emerge with the `--nodeps`
    option, skipping dependency calculation and forcing each atom to be
    blindly rebuilt.
    
    emerge -e --nodeps --oneshot @system
    emerge -e --nodeps --oneshot @profile
    emerge -e --nodeps --oneshot @world
    
    Removing this from the backup directories was a stupid oversight
    to save on space when this should have always been included. No amount
    of saved archive size is worth it for the headache this has been!
    Lessons have been (painfully) learned.

--- bin/backup.sh
+++ bin/backup.sh
@@ -15,8 +15,6 @@ rm -rf var/cache/*
 time tar --exclude='var/lib/libvirt/images' \
     --exclude='usr/src' \
     --exclude='tmp/*' \
-    --exclude='var/db/repos' \
-    --exclude='var/db/pkg' \
     -cvf $(cat etc/hostname)-rootfs-$(date +%Y%m%d).tar \
     ${root_dirs[*]} swapfile
 
