a931a19be6fa1247f9991caba0c83ba77124c4b3
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2026-01-06 22:23:15 +0000

    feat: Exclude temp dirs from backup
    Excludes temporary files and certain large but optional files from
    system backups. This also adds a helpful check to ensure the boot
    partition is also mounted so it can be included in the backup.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+if ! mountpoint boot/; then
+    echo '/boot is not mounted!'
+    exit 1;
+fi
+
 root_dirs=($(find -maxdepth 1 -mindepth 1 -type d,l \
     ! -name 'dev' \
     ! -name 'proc' \
@@ -10,11 +15,18 @@ root_dirs=($(find -maxdepth 1 -mindepth 1 -type d,l \
     ! -name 'lost+found' \
 ));
 
+# Clean the cache rather than exclude since this is
+# safe and gives space back to the system.
 rm -rf var/cache/*
 
 time tar --exclude='var/lib/libvirt/images' \
     --exclude='usr/src' \
     --exclude='tmp/*' \
+    --exclude='var/tmp/*' \
+    --exclude='var/spool' \
+    --exclude='__pycache__/*' \
+    --exclude='*\.iso' \
+    --exclude='var/*\.log' \
+    --exclude-vcs \
     -cvf $(cat etc/hostname)-rootfs-$(date +%Y%m%d).tar \
     ${root_dirs[*]} swapfile
-
